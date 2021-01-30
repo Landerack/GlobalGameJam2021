72a4eb297ceced65fd00691b74a3c1aa0e66ad57
Landerack <37659414+Landerack@users.noreply.github.com>
2021-01-31 00:05:01 +0100

    ClothFix(ish) + TriggerBoxBase

--- LostAndFound/Assets/Scripts/triggerbox.cs
+++ LostAndFound/Assets/Scripts/triggerbox.cs
@@ -1,26 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class triggerbox : MonoBehaviour
 {
-    public ParticleSystem Particles;
-    public AudioListener Song;
+    public ParticleSystem Particles;//stop dark particles
+    public GameObject particles;//Spawn Particle Gameobject
+    public AudioSource voice;
+    public AudioSource music;
+    public AudioClip vocal;
+    public AudioClip song;
+
     // Start is called before the first frame update
     void Start()
     {
+        //pause dark particles
+        //spawn particle gameobject on location.
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-    private void OnTriggerEnter(Collider other)
-    {
-        //particle play   
-    }
+ 
     private void OnTriggerExit(Collider other)
     {
         //particle stop
