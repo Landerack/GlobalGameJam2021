dd0eca1ac87c313c2cbf897aacb9cace2ef3d1a9
Landerack <37659414+Landerack@users.noreply.github.com>
2021-01-31 15:03:09 +0100

    beepboop

--- LostAndFound/Assets/Scripts/AntiTriggerBox.cs
+++ LostAndFound/Assets/Scripts/AntiTriggerBox.cs
@@ -8,6 +8,10 @@ public class AntiTriggerBox : MonoBehaviour
     public AudioSource music;
     public AudioClip vocal;
     public AudioClip song;
+    public GameObject NextMemory;
+    public GameObject antiScene;
+
+    public AudioClip RegularSong;
 
     private float vocalplayed = 0f;
     private float timer = 0f;
@@ -29,5 +33,13 @@ public class AntiTriggerBox : MonoBehaviour
             voice.Play();
             vocalplayed += 1f;
         }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        NextMemory.gameObject.SetActive(true);
+        Destroy(antiScene.gameObject);
+        music.clip = RegularSong;
+        music.Play();
     }
 }
